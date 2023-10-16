7ba99d1f7188c117a25802ea7a9e3cc3bb677a2c
lomak <lomako221@mail.ru>
2023-10-16 17:15:34 +0300

    Examples005_ConditionIfElse/

---
+++ Examples005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "yana")
+{
+    Console.WriteLine("Ура, это же Яна!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
