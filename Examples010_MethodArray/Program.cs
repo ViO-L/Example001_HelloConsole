75fe5813f633d41d327a6d02f5c8119715c5bcca
lomak <lomako221@mail.ru>
2023-10-22 23:24:22 +0300

    Added 08_09_10_11

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 41, 9, 101, 123, 41 };
+
+int n = array.Length;
+int find = 41;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
